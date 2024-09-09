cebb14c16b9b6fbf010ce0eb7156eb996e82667d
zulqarnainnaviwala <zulqarnainnaviwala@gmail.com>
2024-09-09 23:53:17 +0500

    created express app & updated important middlewares

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+//Allowed frontend origins in our app
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+//parse json to js-object and limit size
+app.use(express.json({ limit: "16kb" }));
+//parse url encoding and limit size
+// example: https://www.google.com/search?q=zulqarnain+naviwala
+// In URL encoding, spaces are represented by + or %20. In your URL, the space between zulqarnain and naviwala is encoded as +.
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+// This middleware function tells Express to serve static files (like HTML, CSS, JavaScript, images) from the public folder.
+app.use(express.static("public"));
+//Allows you to read cookies sent with client requests
+app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 // require('dotenv').config({path: './env'})
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"; //add this in dev script to make this syntax work : -r dotenv/config --experimental-json-modules
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
-connectDB();
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO db connection failed !!! ", error);
+  });
 
 /*
 // connect db using  IIFE (Immediately Invoked Function Expression) 

4efa594fa432fc506ce75b6ebeeac6ab5e7a1a47
zulqarnainnaviwala <zulqarnainnaviwala@gmail.com>
2024-11-27 00:48:41 +0500

    added subscriptions endpoints

--- src/app.js
+++ src/app.js
@@ -25,11 +25,13 @@ app.use(cookieParser());
 import userRouter from './routes/user.routes.js'
 import playlistRouter from "./routes/playlist.routes.js"
 import videoRouter from "./routes/video.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/subscription", subscriptionRouter)
 
 // http://localhost:8000/api/v1/users/"register|login|..."
 

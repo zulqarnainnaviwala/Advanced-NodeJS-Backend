128dadfcda5cf2bea8e67ec17a4e5820fe9ac30d
zulqarnainnaviwala <zulqarnainnaviwala@gmail.com>
2024-12-02 23:24:55 +0500

    added tweets endpoints

--- src/app.js
+++ src/app.js
@@ -26,12 +26,14 @@ import userRouter from './routes/user.routes.js'
 import playlistRouter from "./routes/playlist.routes.js"
 import videoRouter from "./routes/video.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/subscription", subscriptionRouter)
+app.use("/api/v1/tweet", tweetRouter)
 
 // http://localhost:8000/api/v1/users/"register|login|..."
 

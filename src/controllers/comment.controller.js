140e1e8b2d585ab4897f8a409cde0f750d580c56
zulqarnainnaviwala <zulqarnainnaviwala@gmail.com>
2024-12-06 01:50:23 +0500

    updated addVideComment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -7,6 +7,46 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 const addVideComment = asyncHandler(async (req, res) => {
+    // TODO: add a comment to a video
+    const videoId = req.params.videoId?.trim();
+    const { content } = req.body;
+    const commentedBy = req.user._id
+
+    if (!videoId) {
+        throw new ApiError(400, "video id is required");
+    }
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "invalid video id");
+    }
+    if (!content || !content?.trim()) {
+        throw new ApiError(400, "content is required");
+    }
+
+    const video = await Video.findById(videoId);
+    if (!video) {
+        throw new ApiError(404, "Video not found");
+    }
+
+    try {
+        const comment = await Comment.create({
+            content,
+            video: videoId,
+            commentedBy
+        });
+
+        if (!comment) {
+            throw new ApiError(500, "Failed to create comment");
+        }
+
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(200, comment, "Comment added successfully")
+            );
+
+    } catch (error) {
+        throw new ApiError(500, error?.message || "An error accured while adding the comment");
+    }
 })
 
 const getVideoComments = asyncHandler(async (req, res) => {

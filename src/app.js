39db53751a96631e3371efb8e6325b672b4fa12e
zulqarnainnaviwala <zulqarnainnaviwala@gmail.com>
2024-12-12 21:50:24 +0500

    added dashboard endpoints

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ import tweetRouter from "./routes/tweet.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import dislikeRouter from "./routes/dislike.routes.js"
 import commentRouter from "./routes/comment.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
@@ -40,6 +41,7 @@ app.use("/api/v1/tweet", tweetRouter)
 app.use("/api/v1/like", likeRouter)
 app.use("/api/v1/dislike", dislikeRouter)
 app.use("/api/v1/comment", commentRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 // http://localhost:8000/api/v1/users/"register|login|..."
 

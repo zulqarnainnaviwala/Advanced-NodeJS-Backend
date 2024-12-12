1b51919e9e9d44d0fdda514aa4e62e3486194d2f
zulqarnainnaviwala <zulqarnainnaviwala@gmail.com>
2024-12-12 21:51:32 +0500

    updated getChannelStats controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,6 +8,104 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const userId = req.user?._id;
+    const viewsCount = await Video.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId),
+            },
+        },
+        {
+            $group: {
+                _id: "$owner",
+                totalViews: { $sum: "$views", },
+            },
+        },
+    ]);
+
+    const subscribersCount = await Subscription.aggregate([
+        {
+            $match: {
+                channel: new mongoose.Types.ObjectId(userId),
+            },
+        },
+        {
+            $group: {
+                _id: "$channel",
+                totalSubscribers: { $sum: 1 },
+            }
+        },
+    ]);
+
+    const likeCount = await Like.aggregate([
+        // Lookup video references
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "video"
+            }
+        },
+        // Lookup comment references
+        {
+            $lookup: {
+                from: "comments",
+                localField: "comment",
+                foreignField: "_id",
+                as: "comment"
+            }
+        },
+        // Lookup tweet references
+        {
+            $lookup: {
+                from: "tweets",
+                localField: "tweet",
+                foreignField: "_id",
+                as: "tweet"
+            }
+        },
+        // Match likes where the user has liked video, comment, or tweet
+        {
+            $match: {
+                $or: [
+                    { "video.owner": userId },  // Match if the video is owned by the user
+                    { "comment.commentedBy": userId },  // Match if the comment was made by the user
+                    { "tweet.owner": userId }  // Match if the tweet was posted by the user
+                ]
+            }
+        },
+        // Add fields to count the likes for each content type
+        {
+            $addFields: {
+                videoLikes: { $cond: [{ $gt: [{ $size: "$video" }, 0] }, 1, 0] },  // Count likes for videos
+                commentLikes: { $cond: [{ $gt: [{ $size: "$comment" }, 0] }, 1, 0] },  // Count likes for comments
+                tweetLikes: { $cond: [{ $gt: [{ $size: "$tweet" }, 0] }, 1, 0] }  // Count likes for tweets
+            }
+        },
+        // Group the results to calculate the total counts
+        {
+            $group: {
+                _id: null,  // Group all the documents together
+                videoLikes: { $sum: "$videoLikes" },
+                commentLikes: { $sum: "$commentLikes" },
+                tweetLikes: { $sum: "$tweetLikes" },
+                totalLikes: { $sum: { $add: ["$videoLikes", "$commentLikes", "$tweetLikes"] } }  // Total likes
+            }
+        }
+    ]);
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {
+            // totalVideos: videoCount[0].totalVideos, totalSubscribers: subsCount[0].totalSubscribers
+            totalViews: viewsCount[0].totalViews,
+            totalSubscribers: subscribersCount[0].totalSubscribers,
+            totalLikes: likeCount[0].totalLikes,
+            videoLikes: likeCount[0].videoLikes,
+            commentLikes: likeCount[0].commentLikes,
+            tweetLikes: likeCount[0].tweetLikes
+        }, "Channel Stats Fetched"));
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
